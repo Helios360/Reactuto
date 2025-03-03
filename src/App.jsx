be03386a2293d16d6dd7797b2a3457ae35e959f7
Helios360 <elias.treguier@gmail.com>
2025-03-03 14:20:45 +0100

    Done the footer :3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // App.jsx
 import React from 'react';
 import Header from './components/Header';
-
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
@@ -9,6 +9,7 @@ function App() {
       <main>
         <p>Welcome to my React app!</p>
       </main>
+      <Footer />
     </div>
   );
 }
