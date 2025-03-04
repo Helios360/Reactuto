9b38193ee972e3d2a01206fd7384461758653edf
Helios360 <elias.treguier@gmail.com>
2025-03-04 12:08:24 +0100

    Done the table + cleaned

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,54 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Dish from './components/Dish';
+import Home from './components/Home';
 import mexicanImage from './assets/mexican.jpg';
 import moleImage from './assets/mole.jpg';
 import TACOS from './assets/tacos.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+const dishes = [
+  {
+    id: 1,
+    img: mexicanImage,
+    title: "Mexican Dish",
+    price: 10,
+    isNew: true,
+  },
+  {
+    id: 2,
+    img: moleImage,
+    title: "Mole",
+    price: 10,
+    isNew: false,
+  },
+  {
+    id: 3,
+    img: TACOS,
+    title: "TACOS",
+    price: 10,
+    isNew: false,
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
+        <Home dishes={dishes}/>
         <Container>
           <Row>
-            <Dish
-              img={mexicanImage}
-              title="Mexican Dish"
-              price={10}
-              isNew="true"
+            {dishes.map(dish=>(
+              <Dish
+              key={dish.id}
+              img={dish.img}
+              title={dish.title}
+              price={dish.price}
+              isNew={dish.isNew}
             />
-            <Dish
-              img={moleImage}
-              title="Mole"
-              price={10}
-            />
-            <Dish
-              img={TACOS}
-              title="TACOS"
-              price={10}
-            />
+            ))}
           </Row>
         </Container>
       </main>
--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -2,18 +2,18 @@ import { Card, Col, Badge } from 'react-bootstrap';
 
 const ProductCard = ({ img, title, price, isNew=false }) => {
   return (
-    <Col xs={12} sm={6} md={4} className="mb-4">
-      <Card className="h-100 rounded-3 overflow-hidden">
-        <div className="border-bottom position-relative">
+    <Col xs={12} sm={6} md={4} mb-4>
+      <Card h-100 rounded-3 overflow-hidden>
+        <div border-bottom position-relative>
           <Card.Img 
             variant="top" 
             src={img} 
             alt={title} 
-            style={{ height: '200px', width: '100%', objectFit: 'cover'}} 
+            className="w-100 h-200"
           />
           {isNew && (<Badge bg="secondary" className="position-absolute top-0 end-0 m-2">New</Badge>)}
         </div>
-        <Card.Body className="text-start">
+        <Card.Body text-start>
           <Card.Title as="h3">{title}</Card.Title>
           <Card.Text as="h4">{price}€</Card.Text>
         </Card.Body>
